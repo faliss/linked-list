09f04df4505ba7848e9ba513ad1e8862cd5e2604
faliss <gaal.faliss@gmail.com>
2025-07-06 20:08:48 -0500

    Added the file bubble_sort

---
+++ Project1/bubble_sort.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* createNode(int value) {
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+void insertNode(Node*& head, int value) {
+    Node* newNode = createNode(value);
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void printList(Node* head) {
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+void bubbleSort(Node*& head) {
+    if (head == nullptr || head->next == nullptr) return;
+
+    bool swapped;
+    Node* current;
+    Node* lastSorted = nullptr;
+
+    do {
+        swapped = false;
+        current = head;
+
+        while (current->next != lastSorted) {
+            if (current->data > current->next->data) {
+                // Swap data values
+                int temp = current->data;
+                current->data = current->next->data;
+                current->next->data = temp;
+                swapped = true;
+            }
+            current = current->next;
+        }
+        lastSorted = current;
+    } while (swapped);
+}
+
+int main() {
+    Node* head = nullptr;
+    int value;
+
+    cout << "Enter values for the linked list (enter -1 to stop):\n";
+    while (true) {
+        cin >> value;
+        if (value == -1) break;
+        insertNode(head, value);
+    }
+
+    cout << "\nUnsorted Linked List: ";
+    printList(head);
+
+    bubbleSort(head);
+    cout << "After Bubble Sort: ";
+    printList(head);
+
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
+}
